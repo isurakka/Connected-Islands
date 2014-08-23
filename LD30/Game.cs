77d7b096093fb2491cae495b378dbab719965b0f
Iiro Surakka <iiro.surakka@gmail.com>
2014-08-23 04:37:57 +0300

    Add initial project files

---
+++ LD30/Game.cs
@@ -0,0 +1,36 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD30
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            new Game().Run();
+        }
+
+        RenderWindow rw;
+
+        public Game()
+        {
+            rw = new RenderWindow(new VideoMode(1024, 768), "LD30", Styles.Close, new ContextSettings() { AntialiasingLevel = 8 });
+        }
+
+        public void Run()
+        {
+            while (rw.IsOpen)
+            {
+                rw.DispatchEvents();
+
+                rw.Clear();
+                rw.Display();
+            }
+        }
+    }
+}
